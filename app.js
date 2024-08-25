808a52f9533b569dd21897302904620b50bb8b0f
maharjansuren37 <maharjansuren37@gmail.com>
2024-08-25 20:04:25 +0930

    add app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+function setCssVariables(element, variable, value) {
+    element.style.setProperty(variable, value);
+}
+
+function lightButtonCLick() {
+    setCssVariables(document.documentElement, '--current-color', '#000');
+    setCssVariables(document.documentElement, '--current-background', '#fff');
+}
+
+function darkButtonClick() {
+    setCssVariables(document.documentElement, '--current-color', '#fff');
+    setCssVariables(document.documentElement, '--current-background', '#000');
+}
+
+document.querySelector('#light').addEventListener('click', lightButtonCLick);
+document.querySelector('#dark').addEventListener('click', darkButtonClick);

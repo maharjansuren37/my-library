97e3b3282011990f33bd24503276fb4565fd022c
maharjansuren37 <maharjansuren37@gmail.com>
2024-08-29 06:31:55 +0930

    add app.js

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-function setCssVariables(element, variable, value) {
-    element.style.setProperty(variable, value);
+// function setCssVariables(element, variable, value) {
+//     element.style.setProperty(variable, value);
+// }
+
+// function lightButtonCLick() {
+//     setCssVariables(document.documentElement, '--current-color', '#000');
+//     setCssVariables(document.documentElement, '--current-background', '#fff');
+// }
+
+// function darkButtonClick() {
+//     setCssVariables(document.documentElement, '--current-color', '#fff');
+//     setCssVariables(document.documentElement, '--current-background', '#000');
+// }
+
+// document.querySelector('#light').addEventListener('click', lightButtonCLick);
+// document.querySelector('#dark').addEventListener('click', darkButtonClick);
+
+const button = document.querySelector('button');
+
+function counter() {
+    let num = 0;
+
+    const count = document.getElementById('counter');
+
+    count.textContent = "number";
 }
 
-function lightButtonCLick() {
-    setCssVariables(document.documentElement, '--current-color', '#000');
-    setCssVariables(document.documentElement, '--current-background', '#fff');
-}
-
-function darkButtonClick() {
-    setCssVariables(document.documentElement, '--current-color', '#fff');
-    setCssVariables(document.documentElement, '--current-background', '#000');
-}
-
-document.querySelector('#light').addEventListener('click', lightButtonCLick);
-document.querySelector('#dark').addEventListener('click', darkButtonClick);
+button.addEventListener('click', counter)
